ac23541419d234e97c13ff0b8410edcc8424d0f7
Tajammal Waheed <44042193+tajammalw@users.noreply.github.com>
2024-08-29 15:13:28 +0000

    Modified index.html and index.js and created fadeIn function for the four assigned sections/elements

---
+++ index.js
@@ -0,0 +1,21 @@
+// Created fadeIn function to fade in the html elements: 
+// The food truck icon, quarterly menu, favorite section, and location and hours sections.
+
+function fadeIn(element, increment, intervalTime) {
+    let opacity = 0; // Initial opacity
+    let interval = setInterval(function() {
+        if (opacity < 1) {
+            opacity += increment;
+            element.style.opacity = opacity;
+        } else {
+            clearInterval(interval); // Stop the interval when opacity reaches 1
+        }
+    }, intervalTime);
+ }
+ 
+ // Elements to fade in with their respective increments
+ fadeIn(document.querySelector('#hero'), 0.007, 30);
+ fadeIn(document.querySelector('#menu'), 0.007, 30);
+ fadeIn(document.querySelector('#favorites'), 0.007, 30);
+ fadeIn(document.querySelector('#location'), 0.007, 30);
+ fadeIn(document.querySelector('#hours'), 0.007, 30);
